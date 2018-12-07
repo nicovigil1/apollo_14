45c145dd2fde18ec94dfba2644e2b4f665fdf6c7
Nico <nicovigil1@gmail.com>
2018-12-07 10:49:00 -0700

    adds model and feature test for displaying and calculating average age of the astronauts

--- spec/feature/user_can_see_astronauts_spec.rb
+++ spec/feature/user_can_see_astronauts_spec.rb
@@ -13,6 +13,14 @@ RSpec.describe 'as a visitor' do
             expect(page).to have_content("Job: Piano Mam")
             expect(page).to have_content("Job: Tiny Dancer")
         end 
+
+        it 'can see average age of the astronauts' do 
+            Astronaut.create(name:"Billy Joel", age:"10", job:"Piano Man")
+            Astronaut.create(name:"Elton John", age:"20", job:"Tiny Dancer")
+           
+
+            expect(page).to have_content("Average Age: 15")
+        end 
     end
 end
 
--- spec/models/astronaut_spec.rb
+++ spec/models/astronaut_spec.rb
@@ -11,4 +11,13 @@ describe Astronaut, type: :model do
     it { should have_many :astronaut_missions}
     it { should have_many :missions}
   end
+
+  describe 'Analyitics' do
+    it 'can calculate average age' do
+      Astronaut.create(name:"Billy Joel", age:"10", job:"Piano Man")
+      Astronaut.create(name:"Elton John", age:"20", job:"Tiny Dancer")
+
+      expect(Astronaut.avg_age).to eq(15)
+    end 
+  end 
 end
